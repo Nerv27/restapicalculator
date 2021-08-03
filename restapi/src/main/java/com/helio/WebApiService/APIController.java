1985dd92bac10108ac5695853eddd95c216ec339
Helio.Manganhela <Helio.Manganhela@standardbank.co.mz>
2021-08-03 11:39:35 +0200

    Refactored code for logging and logic flow

--- restapi/src/main/java/com/helio/WebApiService/APIController.java
+++ restapi/src/main/java/com/helio/WebApiService/APIController.java
@@ -36,35 +36,35 @@ public class APIController {
         int reqid = new Random().nextInt(Integer.MAX_VALUE);
         HashMap map = new HashMap<>();
 
-        Calcapilogger.info("Request received for Caculator API");
+        Calcapilogger.info("Request received for Calculator API");
         try {
-            Calcapilogger.info("Request is for caculation of type " + signcalculation);
+            /*Create object of calculator set values*/
             Calcapilogger.info("Request id is " + reqid);
-            /*CrCalcapilogger.info("Request is for caculation of type " + signcalculation);eate object of calculator set values*/
             RequestOB calobj = new RequestOB(avalue, bvalue, Result, signcalculation);
             signcalculation = calobj.getsignfrompath(restOfTheUrl); // identify the type of calculation to via the url path used
+            Calcapilogger.info("Request id: " + reqid + " - Request is for calculation of type " + signcalculation);
             Calcapilogger.info("Request id: " + reqid + " - Request pre processing done successfuly.");
             Calcapilogger.info("Request id: " + reqid + " - Request being sent to calculator. Using the value of A = " + calobj.getNumA().toString()
                     +" and value of B = " + calobj.getNumB().toString());
             CalculatorService calculationrequest = new CalculatorService(avalue, bvalue, signcalculation);
             Calcapilogger.info("Request id: " + reqid + " - Request calculated successfuly.");
             calobj.setResult(calculationrequest.CalculationResult());
-            //calobj.setSign(signcalculation);
+
 
             /* Sending to Message Queue, wait then get response */
+            Calcapilogger.info("Request id: " + reqid + " - Request result is " + calobj.getResult().toString());
+            responseHeaders.set("Request_ID", reqid + ""); //set unique id of request as new header key
+            Calcapilogger.info("Request id: " + reqid + " - Sending response to client");
+
             /*map.put("requestid", reqid);
             map.put("valueA", calobj.getNumA());
             map.put("valueB", calobj.getNumB());
             map.put("sign", calobj.getSign());*/
             map.put("Result", calobj.getResult());
-            Calcapilogger.info("Request id: " + reqid + " - Request result is " + calobj.getResult().toString());
-            responseHeaders.set("RequestID", reqid+""); //set unique id of request as new header key
-
-            Calcapilogger.info("Request id: " + reqid + " - Sending response to client");
             return new ResponseEntity<HashMap>( map , responseHeaders, HttpStatus.OK);
 
         } catch (Exception ex) {
-            Calcapilogger.error("Request id: " + reqid + " - Request received for Caculator API is bad request. Error: ", ex);
+            Calcapilogger.error("Request id: " + reqid + " - Request received for Calculator API is bad request. Error: ", ex);
             return new ResponseEntity<String>(AppConstant.MESSAGE_QUEUE_SEND_ERROR,
                     HttpStatus.INTERNAL_SERVER_ERROR);
         }
